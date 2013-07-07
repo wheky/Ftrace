13122e0796e018a51e6c1893449b185011ba5b60
ayzz <yoannm57@gmail.com>
2013-07-07 18:54:34 +0200

    libpath include

---
+++ include/libpath.h
@@ -0,0 +1,14 @@
+/*
+** libpath.h for  in /home/masson_y/prog/tech2/adsysunix/Ftrace/include
+** 
+** Made by 
+** Login   <masson_y@epitech.net>
+** 
+** Started on  Sun Jul 07 18:52:54 2013 
+** Last update Sun Jul 07 18:52:54 2013 
+*/
+
+#ifndef __LIBPATH__
+#define __LIBPATH__
+char 		**get_shared_libpath(pid_t pid);
+#endif
